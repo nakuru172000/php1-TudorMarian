b206720b1cccdfdbb1bda83bd68d71ee102f9979
nakuru172000 <150978501+nakuru172000@users.noreply.github.com>
2025-02-13 19:53:18 +0100

    ex 1 e 2 terminati

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Traccia 1
+$prova="ciao Mondo";
+echo($prova);
+var_dump($prova);
+$ciccioBombaPeso=160;
+$ciccioBombaH=5.7;
+$ciccioBombaGenio=false;
+$ciccioBombaCognome="Atomic";
+const CICCIOBOMBAPESO=160;
+const CICCIOBOMBAH=5.7;
+const CICCIOBOMBAGENIO=false;
+const CICCIOBOMBACOGNOME="Atomic";
+var_dump($ciccioBombaPeso);
+var_dump($ciccioBombaH);
+var_dump($ciccioBombaGenio);
+var_dump($ciccioBombaCognome);
+
+
+
+// Traccia 2
+$text = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = '$text2';
+$text7 = 'bevuto';
+$text8 = "tutto";
+echo ("$text $text2 $text3$text4 $text5 $text7 $text8.");
